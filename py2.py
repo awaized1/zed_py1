173b32d838350215c7fe47fa160dd5a61d3a9bd5
awaized1 <aduolaayomide@gmail.com>
2025-10-22 23:32:14 +0000

    second commit

---
+++ py2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+name = "Ayomide"
+level = 'A'
+age = 20
+percentage = 70.48
+
+print ("My name is {}. I am of level {}. I am {} years old and I had {}%.".format(name, level, age, percentage))
